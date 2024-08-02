d2a6be9f1555647a9af190e032eeb3bc5719c37a
spblue4422 <spblue4422@naver.com>
2024-08-02 17:15:30 +0900

    rename function name

--- src/apis/divePoint/divePoint.controller.ts
+++ src/apis/divePoint/divePoint.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { DivePointService } from './divePoint.service';
 import { PaginationReqDto } from 'src/common/dtos/paginationReq.dto';
 import { MsgResDto } from 'src/common/dtos/msgRes.dto';
+import { CurrentUser } from 'src/common/decorators/currentUser';
+import { JwtAccessPayloadDto } from 'src/common/dtos/jwtPayload.dto';
 
 @Controller('point')
 export class DivePointController {
@@ -19,8 +21,13 @@ export class DivePointController {
     return this.divePointService.getDivePoint(pointId);
   }
 
-  @Patch('/recomment/:pointId')
-  async recommentPoint(@Param() pointId: number, userId: number): Promise<MsgResDto> {
-    return this.divePointService.recommendDivePoint(pointId, userId);
+  @Patch('/recommend/:pointId')
+  async recommendPoint(
+    @Param() pointId: number,
+    @CurrentUser() user: JwtAccessPayloadDto,
+  ): Promise<MsgResDto> {
+    const { authId } = user;
+
+    return this.divePointService.recommendDivePoint(pointId, authId);
   }
 }
